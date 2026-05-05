c5ea40e0f0c174e3b40353befa51e9407c3777df
Eastwood Tweneboah Osei <eastwoodosei0@gmail.com>
2026-05-05 22:15:12 +0000

    Fix: Normalize auth header name

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+    config.headers['authorization'] = `Bearer ${token}`;
   }
   return config;
 });
